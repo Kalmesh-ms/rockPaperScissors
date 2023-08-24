7b484bb7f9c572693115b1d7d19852636435368f
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-08-24 20:43:35 +0530

    Propmt for player choice & add playRound func for game result

--- myscript.js
+++ myscript.js
@@ -15,3 +15,31 @@ function getComputerChoice(randomNumber){
 
 cs = getComputerChoice(randomNumber);
 
+
+playerSelection = prompt("Please input your selection").toLowerCase();
+ps = playerSelection;
+
+
+function lossLogic(cs , ps){
+    if (( cs === "rock" && ps === "scissors") || (cs === "scissors" && ps === "paper") || (cs === "paper" && ps === "rock")){
+    return true
+    }
+    else {return false}
+}
+
+function playRound (ps , cs){
+
+    if ( lossLogic(cs, ps )){
+        return `You lose! ${cs} beats ${ps}`
+    }
+
+    else if (ps === cs ){
+        return `Its a draw`
+    }
+    else {
+        return `You win ! ${ps} beats ${cs}`
+    }
+}
+
+console.log(playRound(ps,cs));
+
