9d49e9d95e953b8b45d0b89b8ffafd11e0ec76a0
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-09-01 16:38:43 +0530

    Display text content by event listener for each playRound

--- myscript.js
+++ myscript.js
@@ -1,56 +1,72 @@
 let computerPoints = 0;
 let playerPoints = 0;
 
-let randomNumber = Math.floor(Math.random()*10);
 
-function getComputerChoice(randomNumber){
-    
-    if (randomNumber > 6){
-        return "rock"
-    }
-    else if (randomNumber < 3 ){
-        return "scissors"
-    }
-    else {
-        return "paper"
-    }
-}
 
-cs = getComputerChoice(randomNumber);
 
-function lossLogic(cs , ps){
-    if (( cs === "rock" && ps === "scissors") || (cs === "scissors" && ps === "paper") || (cs === "paper" && ps === "rock")){
-    return true
-    }
-    else {return false}
-}
+const container =  document.querySelector('div');
 
-function playRound (ps , cs){
+const eachPlayText = document.createElement('div');
+eachPlayText.classList.add('play')
 
-    if ( lossLogic(cs, ps )){
-        computerPoints++
-        return `You lose! ${cs} beats ${ps}`
-    }
+const computerScore = document.createElement('div');
+computerScore.classList.add('cscore');
 
-    else if (ps === cs ){
-        return `Its a draw`
-    }
-    else {
-        playerPoints++
-        return `You win ! ${ps} beats ${cs}`
-    }
-}
-
-const contianer =  document.querySelector('div');
-const div = document.createElement('div');
+const playerScore = document.createElement('div');
+playerScore.classList.add('pscore');
 
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((button)=>
-button.addEventListener('click',(e)=>{
+function listen(e){
     ps = e.target.className;
-    play = playRound(ps , cs);
+        
+    let randomNumber = Math.floor(Math.random()*10);
+
+            function getComputerChoice(randomNumber){
+                
+                if (randomNumber > 6){
+                    return "rock"
+                }
+                else if (randomNumber < 3 ){
+                    return "scissors"
+                }
+                else {
+                    return "paper"
+                }
+            }
+
+    cs = getComputerChoice(randomNumber);
+
+            function lossLogic(cs , ps){
+                if (( cs === "rock" && ps === "scissors") || (cs === "scissors" && ps === "paper") || (cs === "paper" && ps === "rock")){
+                return true
+                }
+                else {return false}
+            }
+        
+            playRound = function (ps , cs){
+            
+                if ( lossLogic(cs, ps )){
+                    computerPoints++
+                    return `You lose! ${cs} beats ${ps}`
+                }
+            
+                else if (ps === cs ){
+                    return `Its a draw`
+                }
+                else {
+                    playerPoints++
+                    return `You win ! ${ps} beats ${cs}`
+                }
+            }
+    eachPlayText.textContent = playRound(ps,cs);
+    playerScore.textContent =`You : ${playerPoints}`;
+    computerScore.textContent = `Computer : ${computerPoints}`;
 }
 
-))
+buttons.forEach((button)=>
+button.addEventListener('click' , listen))
 
+container.appendChild(eachPlayText);
+container.appendChild(playerScore);
+container.appendChild(computerScore);
